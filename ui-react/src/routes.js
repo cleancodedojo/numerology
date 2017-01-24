a0a4a5154111144f18e2aec8d1e3ebb0d54ff782
raydecastro <captray108@gmail.com>
2017-01-23 17:37:28 -0800

    cleanup. removed some unused code.

--- ui-react/src/routes.js
+++ ui-react/src/routes.js
@@ -3,7 +3,6 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import HomePage from './components/HomePage';
-import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
 import AboutPage from './components/AboutPage';
 import NotFoundPage from './components/NotFoundPage';
 import NumerologyPage from './components/NumerologyPage';
@@ -12,7 +11,6 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="numerology" component={NumerologyPage}/>
-    <Route path="fuel-savings" component={FuelSavingsPage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
