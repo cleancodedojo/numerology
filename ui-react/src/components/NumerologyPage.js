344d2bdd7616b885e9411d1b3eab242312839064
raydecastro <captray108@gmail.com>
2017-01-24 16:15:49 -0800

    feature. implemented preliminary ui. (still incomplete)

--- ui-react/src/components/NumerologyPage.js
+++ ui-react/src/components/NumerologyPage.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/numerology.css';
 
 const NumerologyPage = () => {
   return (
-    <div className="numerology-page">
-      <h1>Numerology</h1>
-      <h3>Firstname</h3>
-      <input type="text" name="firstname" />
-      <h3>Lastname</h3>
-      <input type="text" name="lastname" />
-      <br />
-      <button className="button-submit" type="submit">magic</button>
+    <div className="numerology-page container-fluid">
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">Numerology</h3>
+        </div>
+        <div className="panel-body">
+          <div className="input-group col-lg-8">
+            <span className="input-group-addon" id="handle">@</span>
+            <input type="text" className="form-control" placeholder="handle" />
+          </div>
+          <div className="input-group col-lg-8">
+            <label htmlFor="email-address">Email Address</label>
+            <input type="text" className="form-control" id="email-address" />
+          </div>
+          <div className="input-group col-lg-8">
+            <label htmlFor="first-name">First Name</label>
+            <input type="text" className="form-control" id="first-name" />
+          </div>
+          <div className="input-group col-lg-8">
+            <label htmlFor="middle-name">Middle Name</label>
+            <input type="text" className="form-control" id="middle-name" />
+          </div>
+          <div className="input-group col-lg-8">
+            <label htmlFor="last-name">Last Name</label>
+            <input type="text" className="form-control" id="last-name" />
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn-default">
+              calculate
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
