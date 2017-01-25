e05c22689601d435241a282095dbf2980832810c
raydecastro <captray108@gmail.com>
2017-01-24 16:59:03 -0800

    feature. implemented firstname, lastname field properties and submit events.

--- ui-react/src/components/NumerologyPage.js
+++ ui-react/src/components/NumerologyPage.js
@@ -1,44 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/numerology.css';
 
-const NumerologyPage = () => {
-  return (
-    <div className="numerology-page container-fluid">
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <h3 className="panel-title">Numerology</h3>
-        </div>
-        <div className="panel-body">
-          <div className="input-group col-lg-8">
-            <span className="input-group-addon" id="handle">@</span>
-            <input type="text" className="form-control" placeholder="handle" />
+class NumerologyPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  handleFirstNameChange(event) {
+    console.log('fn: ' + event.target.value);
+    this.setState({firstName: event.target.value});
+  }
+
+  handleLastNameChange(event) {
+    this.setState({lastName: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="numerology-page container-fluid">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">Numerology</h3>
           </div>
-          <div className="input-group col-lg-8">
-            <label htmlFor="email-address">Email Address</label>
-            <input type="text" className="form-control" id="email-address" placeholder="email@happiness.com" />
-          </div>
-          <div className="input-group col-lg-8">
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" className="form-control" id="first-name" placeholder="first name given during birth"/>
-          </div>
-          <div className="input-group col-lg-8">
-            <label htmlFor="middle-name">Middle Name</label>
-            <input type="text" className="form-control" id="middle-name" />
-          </div>
-          <div className="input-group col-lg-8">
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" className="form-control" id="last-name" placeholder="last name given during birth"/>
-          </div>
-          <div className="btn-group">
-            <button type="submit" className="btn btn-default">
-              calculate
-            </button>
+          <div className="panel-body">
+            <form onSubmit={this.handleSubmit}>
+              <div className="input-group col-lg-8">
+                <span className="input-group-addon" id="handle">@</span>
+                <input type="text" className="form-control" placeholder="handle"/>
+              </div>
+              <div className="input-group col-lg-8">
+                <label htmlFor="email-address">Email Address</label>
+                <input type="text" className="form-control" id="email-address" placeholder="email@happiness.com"/>
+              </div>
+              <div className="input-group col-lg-8">
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" className="form-control" id="first-name"
+                       placeholder="first name given during birth"
+                       value={this.state.firstName} onChange={this.handleFirstNameChange} />
+              </div>
+              <div className="input-group col-lg-8">
+                <label htmlFor="middle-name">Middle Name</label>
+                <input type="text" className="form-control" id="middle-name"/>
+              </div>
+              <div className="input-group col-lg-8">
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" className="form-control" id="last-name" placeholder="last name given during birth"
+                       value={this.state.lastName} onChange={this.handleLastNameChange} />
+              </div>
+              <div className="btn-group">
+                <button type="submit" className="btn btn-default">
+                  calculate
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default NumerologyPage;
